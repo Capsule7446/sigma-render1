a019bba7f1734b27c3377976eff47451b979d3d7
zPaul <paullai0201@gmail.com>
2023-11-29 12:03:59 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -11,16 +11,5 @@ export default defineConfig({
       formats: ['umd']
     },
     sourcemap: false, // 输出.map文件
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-          helloword: 'helloword'   // 这里暴露出去一个全局变量
-        }
-      }
-    }
   }
 })
